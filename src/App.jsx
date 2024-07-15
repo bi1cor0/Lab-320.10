feca97099880945c1e0cc2dad5f24f301f78ac19
yeahbudd01 <billcorcoro@comcast.net>
2024-07-14 23:56:29 -0700

    Setting up some boilerplates for the to do list.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
+//imports
 import { useState, useReducer } from 'react'
-
 import './App.css'
 
-function App() {
+//functions
+function reducer(reducState, action){
 
+}
+
+export default function App() {
+
+  const [state, dispatch] = useReducer(reducer, {})
+  const [name, setName] = useState("")
 
   return (
     <>
-      
+      <form>
+        <input type='text'
+        value={name}
+        onChange={e => setName(e.target.value)}/>
+      </form>
     </>
   )
 }
 
-export default App
+ 
