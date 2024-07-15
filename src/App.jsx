2aa3981781da07ad0f7434d42a36d3cc878c6fb6
yeahbudd01 <billcorcoro@comcast.net>
2024-07-15 01:12:20 -0700

    Added functions from useReducer for user input text.

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,36 @@ import { useState, useReducer } from 'react'
 import './App.css'
 
 //functions
-function reducer(reducState, action){
 
+function newtoDo(name) {
+  return { id: Date.now(), name: name, complete: false }
 }
+
+function reducer(currtoDos, action){
+  switch (action.type) {
+    case 'add-toDo':
+      return[currtoDos, newtoDo(action.payload.name)]
+  }
+}
+
 
 export default function App() {
 
   const [state, dispatch] = useReducer(reducer, {})
   const [name, setName] = useState("")
 
+  function actSubmit(e){
+    e.preventDefault()
+    dispatch({ type: 'add-toDo', payload: {name: name}})
+    setName('')
+  }
+  
+
+  console.log(state)
+
   return (
     <>
-      <form>
+      <form onSubmit={actSubmit}> 
         <input type='text'
         value={name}
         onChange={e => setName(e.target.value)}/>
